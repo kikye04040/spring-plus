ca5e78afde9444db75f3100289519803eaf91bfc
kikye04040 <kikye04040@gmail.com>
2024-10-06 02:04:20 +0900

    level 2-9: fromAuthUser 부분 변경

--- src/main/java/org/example/expert/domain/comment/service/CommentService.java
+++ src/main/java/org/example/expert/domain/comment/service/CommentService.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.comment.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.expert.config.CustomUserDetails;
 import org.example.expert.domain.comment.dto.request.CommentSaveRequest;
 import org.example.expert.domain.comment.dto.response.CommentResponse;
 import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
@@ -27,7 +28,7 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     @Transactional
-    public CommentSaveResponse saveComment(AuthUser authUser, long todoId, CommentSaveRequest commentSaveRequest) {
+    public CommentSaveResponse saveComment(CustomUserDetails authUser, long todoId, CommentSaveRequest commentSaveRequest) {
         User user = User.fromAuthUser(authUser);
         Todo todo = todoRepository.findById(todoId).orElseThrow(() ->
                 new InvalidRequestException("Todo not found"));
