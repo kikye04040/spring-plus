fa5d1e6081655fe8d3e9821d1b336cb99c69c1d2
kikye04040 <kikye04040@gmail.com>
2024-10-06 02:04:07 +0900

    level 2-9: User 엔티티 fromAuthUser 부분 변경

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.user.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.expert.config.CustomUserDetails;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.entity.Timestamped;
 import org.example.expert.domain.user.enums.UserRole;
@@ -36,8 +37,8 @@ public class User extends Timestamped {
         this.nickname = nickname;
     }
 
-    public static User fromAuthUser(AuthUser authUser) {
-        return new User(authUser.getId(), authUser.getEmail(), authUser.getUserRole(), authUser.getNickname());
+    public static User fromAuthUser(CustomUserDetails customUserDetails) {
+        return new User(customUserDetails.getId(), customUserDetails.getEmail(), customUserDetails.getUserRole(), customUserDetails.getNickname());
     }
 
     public void changePassword(String password) {
